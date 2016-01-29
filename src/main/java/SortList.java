5ba7708959f16f1e3918889d9e2f8b970a67e356
Andressa Sivolella <asivolella@thoughtworks.com>
2016-01-29 15:42:37 -0200

    Add N (userNumber) to a raw list

--- src/main/java/SortList.java
+++ src/main/java/SortList.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Created by asivolella <Andressa Sivolella> on 2016-01-29.
  */
 public class SortList {
     private int userNumber;
+    private List<Integer> rawList = new ArrayList<>();
 
     public void setUserNumber(int userNumber) {
         this.userNumber = userNumber;
     }
 
     public int getUserNumber() {
-        return userNumber;
+        return this.userNumber;
+    }
+
+    public List<Integer> getRawList() {
+        return this.rawList;
+    }
+
+    public void addRawListElement(int number) {
+        this.rawList.add(number);
+    }
+
+    public int rawListSize() {
+        return this.rawList.size();
     }
 }
--- src/test/java/SortListTest.java
+++ src/test/java/SortListTest.java
@@ -1,12 +1,20 @@
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /*
  * Created by asivolella <Andressa Sivolella> on 2016-01-29.
  */
 public class SortListTest {
     SortList sort;
+    private int userNumber;
 
     @Before
     public void setUp(){
@@ -15,10 +23,36 @@ public class SortListTest {
 
     @Test
     public void shouldSetUserNumber(){
-        int expectedUserNumber = 5;
-        this.sort.setUserNumber(5);
+        int expectedUserNumber = this.userNumber;
+        this.sort.setUserNumber(this.userNumber);
         int actualUserNumber = this.sort.getUserNumber();
 
         assertEquals("SortList.setUserNumber is not setting userNumber correctly", expectedUserNumber, actualUserNumber);
+    }
+
+    @Test
+    public void shouldAddNnumberInAList(){
+        List<Integer> expectedList = Arrays.asList(12, 1);
+
+        this.sort.addRawListElement(12);
+        this.sort.addRawListElement(1);
+
+        List<Integer> actualList = this.sort.getRawList();
+
+        assertThat(actualList, is(expectedList));
+    }
+
+    @Test
+    public void userNumberShouldBeEqualToRawListSize(){
+        int expectedSize = 3;
+        this.sort.setUserNumber(3);
+
+        this.sort.addRawListElement(12);
+        this.sort.addRawListElement(1);
+        this.sort.addRawListElement(25);
+
+        int actualSize = this.sort.rawListSize();
+
+        assertEquals("SortList.addRawListElement is not adding numbers correctly", expectedSize, actualSize);
     }
 }
